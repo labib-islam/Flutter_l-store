ca7fea32d4ee1503561506701c785c57382d6656
labib-islam <labibislam87@gmail.com>
2024-07-27 20:23:47 +0600

    promo-slider_product-card_frontend

---
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_l_store/common/styles/shadows.dart';
+import 'package:flutter_l_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:flutter_l_store/common/widgets/images/l_rounded_image.dart';
+import 'package:flutter_l_store/utils/constants/colors.dart';
+import 'package:flutter_l_store/utils/constants/image_strings.dart';
+import 'package:flutter_l_store/utils/constants/sizes.dart';
+import 'package:flutter_l_store/utils/helpers/helper_functions.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../icons/l_circular_icon.dart';
+import '../../texts/product_price_text.dart';
+import '../../texts/product_title_text.dart';
+
+class LProductCardVertical extends StatelessWidget {
+  const LProductCardVertical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = LHelperFunctions.isDarkMode(context);
+
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        width: 180,
+        padding: const EdgeInsets.all(1),
+        decoration: BoxDecoration(
+          boxShadow: [LShadowStyle.verticalProductShadow],
+          borderRadius: BorderRadius.circular(LSizes.productImageRadius),
+          color: dark ? LColors.darkerGrey : LColors.white,
+        ),
+        child: Column(
+          children: [
+            /// Thumbnail, Wishlist Button, Discount Tag
+            LRoundedContainer(
+              height: 180,
+              padding: const EdgeInsets.all(LSizes.sm),
+              backgroundColor: dark ? LColors.dark : LColors.light,
+              child: Stack(
+                children: [
+                  /// -- Thumbnail Image
+                  const LRoundedImage(
+                    imageUrl: LImages.productImage1,
+                    applyImageRadius: true,
+                  ),
+
+                  /// -- Sale Tag
+                  Positioned(
+                    top: 12,
+                    child: LRoundedContainer(
+                      radius: LSizes.sm,
+                      backgroundColor: LColors.secondary.withOpacity(0.8),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: LSizes.sm, vertical: LSizes.xs),
+                      child: Text(
+                        '25%',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelLarge!
+                            .apply(color: LColors.black),
+                      ),
+                    ),
+                  ),
+
+                  /// -- Favorite Icon Button
+                  const Positioned(
+                    top: 0,
+                    right: 0,
+                    child: LCircularIcon(
+                      icon: Iconsax.heart5,
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: LSizes.spaceBtwItems / 2,
+            ),
+
+            /// -- Details
+            Padding(
+              padding: const EdgeInsets.only(left: LSizes.sm),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const LProductTitleText(
+                    title: 'Green Nike Air Shoes',
+                    smallSize: true,
+                  ),
+                  const SizedBox(
+                    height: LSizes.spaceBtwItems / 2,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Nike',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                      const SizedBox(
+                        width: LSizes.xs,
+                      ),
+                      const Icon(
+                        Iconsax.verify5,
+                        color: LColors.primary,
+                        size: LSizes.iconXs,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      /// -- Price
+                      const LProductPriceText(price: '35.0',),
+                      Container(
+                        decoration: const BoxDecoration(
+                            color: LColors.dark,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(LSizes.cardRadiusMd),
+                              bottomRight:
+                                  Radius.circular(LSizes.productImageRadius),
+                            )),
+                        child: const SizedBox(
+                            width: LSizes.iconLg * 1.2,
+                            height: LSizes.iconLg * 1.2,
+                            child: Center(
+                              child: Icon(
+                                Iconsax.add,
+                                color: LColors.white,
+                              ),
+                            )),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/navigation_menu.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_l_store/utils/constants/colors.dart';
+import 'package:flutter_l_store/utils/helpers/helper_functions.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+import 'features/shop/screens/home/home.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final darkMode = LHelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.selectedIndex.value = index,
+          backgroundColor: darkMode ? LColors.black : LColors.white,
+          indicatorColor: darkMode
+              ? LColors.white.withOpacity(0.1)
+              : LColors.black.withOpacity(0.1),
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
+            NavigationDestination(icon: Icon(Iconsax.user), label: "Profile"),
+          ],
+        ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    const HomeScreen(),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.deepPurple,
+    ),
+    Container(
+      color: Colors.orange,
+    ),
+  ];
+}
